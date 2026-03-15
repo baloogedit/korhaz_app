2ac25afcebcec884d8368a37fce744640a3262d6
baloogedit <baloogedit@gmail.com>
2026-03-15 19:03:05 +0200

    Add ProfileTab and ProgramTab widgets for user profile and program display

---
+++ lib/tabs/program_tab.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ProgramTab extends StatelessWidget {
+  const ProgramTab({super.key});
+  @override
+  Widget build(BuildContext context) => const Center(child: Text("Program"));
+}
