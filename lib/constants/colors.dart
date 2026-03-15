bf016ca34359c627c2eb90b027a3758925638464
baloogedit <baloogedit@gmail.com>
2026-03-15 19:02:51 +0200

    Add AppColors class for centralized color management

---
+++ lib/constants/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Theme & Buttons
+  static const Color primaryTeal = Color.fromARGB(255, 20, 199, 181);
+  static const Color buttonTeal = Color.fromARGB(255, 23, 109, 116);
+  
+  // Backgrounds
+  static const Color bgTeal = Color.fromARGB(255, 192, 246, 228); // The light grey app background
+  static const Color cardWhite = Color.fromARGB(255, 206, 234, 231);
+  static const Color inputFill = Color(0xFFFAFAFA); // Colors.grey.shade50
+  
+  // Header Gradient
+  static const Color headerGradientStart = Color(0xFF1E5665); // Dark teal
+  static const Color headerGradientEnd = Color(0xFF268585);   // Lighter teal
+  
+  // Bottom Navigation
+  static const Color navIndicator = Color(0xFFB2DFDB); // Colors.teal.shade100
+  
+  // Texts & Icons
+  static const Color textWhite = Colors.white;
+  static const Color textWhiteFaded = Colors.white70;
+  static const Color textGrey = Colors.grey;
+}
