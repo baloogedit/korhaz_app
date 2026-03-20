a91cbb3f5931574178dc40658cf787201c71c3ca
baloogedit <baloogedit@gmail.com>
2026-03-20 15:28:25 +0200

    Fix primaryTeal color definition and reorganize status & alert colors in AppColors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // Theme & Buttons
-  static const Color primaryTeal = Color.fromARGB(255, 20, 199, 181);
+  static const Color primaryTeal = Color(0xFF009688);
   static const Color buttonTeal = Color.fromARGB(255, 23, 109, 116);
   
   // Backgrounds
@@ -21,4 +21,12 @@ class AppColors {
   static const Color textWhite = Colors.white;
   static const Color textWhiteFaded = Colors.white70;
   static const Color textGrey = Colors.grey;
+
+  //status & alerts
+  static const Color successGreen = Color.fromARGB(255, 23, 116, 26);
+  static const Color successLight = Color.fromARGB(255, 178, 226, 182); // For the checked-in icon background
+  static const Color errorRed = Color.fromARGB(255, 144, 14, 12);
+
+
+
 }
